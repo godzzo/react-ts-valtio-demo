b906898580a29dd4229086d7bc7ee6f558aee8f6
Gindilla Péter <godzzo@godzzo.hu>
2023-02-27 06:21:51 +0100

    change image size

--- src/views/SuperHeroDemo.tsx
+++ src/views/SuperHeroDemo.tsx
@@ -3,31 +3,60 @@ import { useSnapshot } from 'valtio';
 import { Box, Column, Row } from '../components/Layout';
 import { SuperHero, superState } from '../store/super-store';
 
+const modes = ['xs', 'sm', 'md', 'lg'];
+
 export default function SuperHeroDemo() {
 	// Working snapshot deconstruct - as state 😃
 	const snap = useSnapshot(superState);
 	const { items, current, select } = snap;
 
+	const [mode, setMode] = useState<'xs' | 'sm' | 'md' | 'lg'>('md');
+
 	return (
 		<>
 			<Row>
-				<Column>
+				<Column
+					style={{
+						maxHeight: '85vh',
+						overflowY: 'auto',
+					}}
+				>
 					{items.map((p) => {
 						// TODO: readonly props because of valtio snapshot
-						return <ItemRow key={p.id} item={p as SuperHero} />;
+						return (
+							<ItemRow
+								key={p.id}
+								item={p as SuperHero}
+								mode={mode}
+							/>
+						);
 					})}
 				</Column>
 				<Column>
 					<Box>
-						<button
-							onClick={() =>
-								select({
-									name: 'New',
-								})
-							}
-						>
-							Create New
-						</button>
+						<Row>
+							<select
+								onChange={(e) => {
+									setMode(e.target.value as any);
+								}}
+								className="p-2"
+							>
+								{modes.map((m) => (
+									<option key={m} value={m}>
+										{m}
+									</option>
+								))}
+							</select>
+							<button
+								onClick={() =>
+									select({
+										name: 'New',
+									})
+								}
+							>
+								Create New
+							</button>
+						</Row>
 					</Box>
 					{current && (
 						<>
@@ -41,9 +70,15 @@ export default function SuperHeroDemo() {
 	);
 }
 
-function ItemRow({ item }: { item: SuperHero }) {
+function ItemRow({
+	item,
+	mode,
+}: {
+	item: SuperHero;
+	mode: 'xs' | 'sm' | 'md' | 'lg';
+}) {
 	return (
-		<Row>
+		<Row style={{ paddingRight: '1rem' }}>
 			<Column>
 				<Row>
 					<Box>Name</Box>
@@ -52,7 +87,7 @@ function ItemRow({ item }: { item: SuperHero }) {
 				<Row>
 					<Box>Picture</Box>
 					<Box>
-						<img src={item.images ? item.images.sm : ''} />
+						<img src={item.images ? item.images[mode] : ''} />
 					</Box>
 				</Row>
 			</Column>
